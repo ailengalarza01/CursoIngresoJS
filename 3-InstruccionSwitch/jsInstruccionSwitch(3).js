cd9b8612708dbbf7edaca050988808385fe9051e
ailengalarza01 <60540083+ailengalarza01@users.noreply.github.com>
2020-02-13 10:26:01 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,17 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+var mes = document.getElementById('mes').value;
+
 	
-	
+	switch(mes){
+        case"Febrero":
+        alert("Este mes no tiene mas de 29 días");
+        break;
 
+        default:
+            alert("Este mes tiene 30 o mas días");
 
-}//FIN DE LA FUNCIÓN
+        }
+}
+//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -4,9 +4,43 @@ function mostrar()
 var laHora = document.getElementById('hora').value;
 
 //alert (laHora);
+
+switch(laHora){
+    case"7":
+    case"8":
+    case"9":
+    case"10":
+    case"11":
+    case"7:00":
+    case"8:00":
+    case"9:00":
+    case"10:00":
+    case"11:00":
+    case"7am":
+    case"8am":
+    case"9am":
+    case"10am":
+    case"11am":
+    case"7.am":
+    case"8.am":
+    case"9.am":
+    case"10.am":
+    case"11.am":
+    case"7:00.am":
+    case"8:00.am":
+    case"9:00.am":
+    case"10:00.am":
+    case"11:00.am":
+    
+    alert("Es de mañana.");
+    break;
+
+    default:
+alert("Para que este ejercicio otorgue informacion acerca del horario, usted debe ingresar una hora entre las 7:00 y las 11:00 en cualquiera de los siguientes formatos: 7-11, 7am-11am, 7.am-11.am,7:00-11:00, 7:00.am-11:00.am o 7:00am-11:00am ")   
+
+
 	
-	
 
 
-
+}
 }//FIN DE LA FUNCIÓN
