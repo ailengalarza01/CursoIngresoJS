824921e44ba72b2aac4f6aa6fb5951469f044746
ailengalarza01 <60540083+ailengalarza01@users.noreply.github.com>
2020-02-19 09:00:28 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -2,6 +2,10 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad
 
+edad=document.getElementById("edad").value;
+if ( edad >= 18) { alert("Usted es una persona mayor de edad.")
+}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 //tomo la edad  
+var edad
 
+edad=document.getElementById("edad").value;
+if ( edad >= 18) { alert("Usted es una persona mayor de edad.")
+}
+else if (edad <18){ alert("Usted es una persona menor de edad.")
+}
 
 }//FIN DE LA FUNCIÓN
